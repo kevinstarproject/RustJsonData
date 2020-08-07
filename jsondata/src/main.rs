0af23a2320c382a5fb455e3b5f57da4478845b96
AzandeLiu <azande.liu@access-company.com>
2020-08-07 18:02:45 +0800

    csv

--- jsondata/src/main.rs
+++ jsondata/src/main.rs
@@ -1,6 +1,9 @@
+extern crate csv;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::Read;
+use std::env;
+use std::error::Error;
 
 #[derive(Serialize, Deserialize)]
 struct Ptt {
@@ -38,17 +41,29 @@ struct PttMessages {
     push_userid: Option<String>
 }
 
-fn main() {
+fn main()  -> Result<(), Box<dyn std::error::Error>>{
+    let mut wtr = csv::Writer::from_path("test.csv")?;
+    wtr.write_record(&["標題","作者","內容","日期","文章類型"])?;
+  
     let mut file = File::open("Gossiping-1-1000.json").unwrap();
     let mut data = String::new();
     file.read_to_string(&mut data).unwrap();
     let ptt: Ptt = serde_json::from_str::<Ptt>(&data).unwrap();
     for elem in ptt.articles{
-        if elem.article_title.is_some() {
-            println!("{}", elem.article_title.unwrap());   
-        }
+        if !elem.error.is_some() && elem.author.is_some() && elem.article_title.is_some() && elem.content.is_some() && elem.date.is_some() {
+      
+            let title =  elem.article_title.unwrap();
+            println!("{}", title);  
+            if title.clone().starts_with("Re:"){
+                wtr.write_record(&[title,elem.author.unwrap(), elem.content.unwrap(), elem.date.unwrap(),String::from("回文")])?;
 
+            }else{
+                wtr.write_record(&[title,elem.author.unwrap(), elem.content.unwrap(), elem.date.unwrap(),String::from("主文")])?;
+
+            }
+                   }
     }
-
- 
+    wtr.flush()?;
+    println!("{}", env::args().nth(3).ok_or("Missing argument")?);
+    Ok(())
 }
