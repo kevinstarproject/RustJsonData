bc391f9f2cfee94a2a6d79c010a892ef95e2a4cb
AzandeLiu <azande.liu@access-company.com>
2020-08-07 14:45:40 +0800

    first

---
+++ jsondata/target/debug/deps/jsondata.d
@@ -0,0 +1,5 @@
+/Users/azandeliu/Downloads/json/RustJsonData/jsondata/target/debug/deps/jsondata: src/main.rs
+
+/Users/azandeliu/Downloads/json/RustJsonData/jsondata/target/debug/deps/jsondata.d: src/main.rs
+
+src/main.rs:
---
+++ jsondata/target/debug/jsondata.d
@@ -0,0 +1,1 @@
+/Users/azandeliu/Downloads/json/RustJsonData/jsondata/target/debug/jsondata: /Users/azandeliu/Downloads/json/RustJsonData/jsondata/src/main.rs
