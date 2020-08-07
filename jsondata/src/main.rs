118bd65d44d130a0b5f641f0249c1daadf8f61e7
AzandeLiu <azande.liu@access-company.com>
2020-08-07 15:15:55 +0800

    json library

--- jsondata/src/main.rs
+++ jsondata/src/main.rs
@@ -1,3 +1,27 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+
+#[derive(Serialize, Deserialize)]
+struct Ptt {
+    name: String,
+    age: u8,
+    phones: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct PttMessagesCount {
+    name: String,
+    age: u8,
+    phones: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct PttMessages {
+    name: String,
+    age: u8,
+    phones: Vec<String>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
